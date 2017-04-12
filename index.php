6c6bbbf3bf923132245e55d49c00c44291b9ae50
YumiTako <yumi.tako.cr@gmail.com>
2017-04-12 15:16:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -24,7 +19,7 @@ and open the template in the editor.
             </ol>
         </div>
         <?php
-        // put your code here
+        
         ?>
     </body>
 </html>
