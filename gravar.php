fea204b7ffc021952192c2fca46a8a9753716605
YumiTako <yumi.tako.cr@gmail.com>
2017-04-12 15:12:11 -0300

    Update gravar.php

--- gravar.php
+++ gravar.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,35 +7,28 @@ and open the template in the editor.
     <body>
         <?php
        
-        //gravação e, banco de dados
-        //1- resgatar dados do form
-        $txtTitulo = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-//Assim você obtém os valores via variável $txtTitulo sem filtro!
-if(isset($txtTitulo["txtTitulo"])){
-        $titulo= $txtTitulo["titulo"];
-        $autor = $txtTitulo["autor"];
-        $categoria = $txtTitulo["categoria"];
-        $editora = $txtTitulo["editora"];
-        $edicao = $txtTitulo["edicao"];
+       
+        $titulo= $_POST["titulo"];
+        $autor = $_POST["autor"];
+        $categoria = $_POST["categoria"];
+        $editora = $_POSTo["editora"];
+        $edicao = $_POST["edicao"];
 }
                 
-        //2-abrir conexao com mysql
+        
         $con = mysqli_connect("localhost","root","","livrosdenis");
         
-        //3-montar a instrução sql de gravação
-        //insert into cliente value(null,'david','david@gmail.com','M')
+       
         $sql= "insert into livros value('".$titulo."','".$autor."','".$categoria."','".$editora."','".$edicao.".)";
         //$sql= "insert into livros value(null,'".$titulo."','".$autor."','".$categoria."','".$editora."','".$edicao."')";
         
-        //echo $sql;
         
-        //4-executar a instrução sql no mysql
         if(mysqli_query($con,$sql)){
             echo"Cadastrado com sucesso!";
         }else{
             echo"erro ao gravar!";
         }
-        //5- fechar a conexão
+        
         mysqli_close($con);   
                        
         ?>
