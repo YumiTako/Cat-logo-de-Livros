904e60fdebe35c9dd0f8dc1ee45b5ce72f603db7
YumiTako <yumi.tako.cr@gmail.com>
2017-04-12 15:15:50 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -49,7 +44,7 @@ and open the template in the editor.
             <input type="submit" value="enviar">
         </form>
         <?php
-        // put your code here
+        
         ?>
     </body>
 </html>
